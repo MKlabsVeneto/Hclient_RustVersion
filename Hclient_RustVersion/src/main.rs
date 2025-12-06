ee2d4846a8f03bf1c080d7b5c9f20f8a2c6f247b
MKlabsVeneto <workmklabsveneto@gmail.com>
2025-12-06 14:47:30 +0100

    modified:   Hclient_RustVersion/src/main.rs 	new file:   Hclient_RustVersion/target/debug/.fingerprint/Hclient_RustVersion-dec72ae7885c8804/output-bin-Hclient_RustVersion 	new file:   Hclient_RustVersion/target/debug/.fingerprint/Hclient_RustVersion-ef493181da4d6f36/output-test-bin-Hclient_RustVersion 	deleted:    Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-0bwxz58gtvwyr/s-hdndwgd6xo-1t5w46q-0sse68u2xkylsepgwo1eomt5s/dep-graph.bin 	deleted:    Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-0bwxz58gtvwyr/s-hdndwgd6xo-1t5w46q-0sse68u2xkylsepgwo1eomt5s/query-cache.bin 	new file:   Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-0bwxz58gtvwyr/s-hdnf1xpkxo-1nn6a91-1r8dit34ka96zbxtxwmzumgun/dep-graph.bin 	new file:   Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-0bwxz58gtvwyr/s-hdnf1xpkxo-1nn6a91-1r8dit34ka96zbxtxwmzumgun/query-cache.bin 	renamed:    Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-0bwxz58gtvwyr/s-hdndwgd6xo-1t5w46q-0sse68u2xkylsepgwo1eomt5s/work-products.bin -> Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-0bwxz58gtvwyr/s-hdnf1xpkxo-1nn6a91-1r8dit34ka96zbxtxwmzumgun/work-products.bin 	renamed:    Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-0bwxz58gtvwyr/s-hdndwgd6xo-1t5w46q.lock -> Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-0bwxz58gtvwyr/s-hdnf1xpkxo-1nn6a91.lock 	deleted:    Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-3jz31jldhzw9j/s-hdndwgd6ug-0kssrjg-eb64z6datr9o59z6t93t8oahn/dep-graph.bin 	deleted:    Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-3jz31jldhzw9j/s-hdndwgd6ug-0kssrjg-eb64z6datr9o59z6t93t8oahn/query-cache.bin 	new file:   Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-3jz31jldhzw9j/s-hdnf1xpkxo-08mwiwi-buqdsbt6bp8gifgh75pok641z/dep-graph.bin 	new file:   Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-3jz31jldhzw9j/s-hdnf1xpkxo-08mwiwi-buqdsbt6bp8gifgh75pok641z/query-cache.bin 	renamed:    Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-3jz31jldhzw9j/s-hdndwgd6ug-0kssrjg-eb64z6datr9o59z6t93t8oahn/work-products.bin -> Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-3jz31jldhzw9j/s-hdnf1xpkxo-08mwiwi-buqdsbt6bp8gifgh75pok641z/work-products.bin     renamed:    Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-3jz31jldhzw9j/s-hdndwgd6ug-0kssrjg.lock -> Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-3jz31jldhzw9j/s-hdnf1xpkxo-08mwiwi.lock 	modified:   Hclient_RustVersion/target/flycheck0/stderr 	modified:   Hclient_RustVersion/target/flycheck0/stdout

--- Hclient_RustVersion/src/main.rs
+++ Hclient_RustVersion/src/main.rs
@@ -1,5 +1,27 @@
-// Hclient Rust Version. a CLI multitool app made by MKlabsVeneto developer team. Kevin De Togni 2025 copyright. 
-
+// COPYRIGHT: Hclient Rust Version, a CLI multi tool rust program for general tech purposes.    Copyright (C) 2025  Kevin De Togni, MKlabs
+// LICENSE: distribuited on the GNU general public license 3.0v license
+// CRATES
+use std::io;
+// MAIN
 fn main() {
-    
+    // LOGO, CREDIT AND FIST PROMPT
+    println!("Hclient Rust Edition, a CLI multi tool rust program for general tech purposes.    Copyright (C) 2025  Kevin De Togni, MKlabs");
+    println!("distribuited on the GNU general public license 3.0v license");
+    println!("          _________    _________");
+    println!("         /        /   /        /");
+    println!("        /        /   /        /");
+    println!("       /        /   /        /");
+    println!("      /        /___/        / _________   ____       ____  _________   ____     ____  _____________           by MKlabs developer team");
+    println!("     /                     / /        /  /   /      /   / /   _____/  /    |   /   / /            /           devs: MrMaxX");
+    println!("    /        ____         / /   _____/  /   /      /   / /   /       /     |  /   / /____    ____/");
+    println!("   /        /   /        / /   /       /   /      /   / /   /____   /      | /   /      /   /                 official website:");
+    println!("  /        /   /        / /   /_____  /   /      /   / /    ____/  /   /|  |/   /      /   /                  officialmklabsveneto.netlify.app");
+    println!(" /        /   /        / /         / /   /____  /   / /    /____  /   / |      /      /   /");
+    println!("/________/   /_______ / /_________/ /________/ /___/ /_________/ /___/  |_____/      /___/                    version: 0.2 BETA");
+    println!("                                          Rust Version");
+    println!();
+    println!("some features only work on linux and its recommended to run Hclient as root or some feature will not work!");
+    println!();
+    println!("slect an option:  = passgen, 2 = btcadressgen, 3 = cardnumbergen, 4 = nmap local ip scan, 5 =  system information, 6 = pkg updater");
+    println!();    
 }
