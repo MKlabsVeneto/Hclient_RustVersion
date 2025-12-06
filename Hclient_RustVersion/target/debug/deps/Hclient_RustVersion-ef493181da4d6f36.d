faf75f3ca0e1458e61f4889923def3a68341bc80
MKlabsVeneto <workmklabsveneto@gmail.com>
2025-12-06 13:59:24 +0100

    new file:   Hclient_RustVersion/Cargo.lock 	new file:   Hclient_RustVersion/Cargo.toml 	new file:   Hclient_RustVersion/src/main.rs 	new file:   Hclient_RustVersion/target/.rustc_info.json 	new file:   Hclient_RustVersion/target/CACHEDIR.TAG 	new file:   Hclient_RustVersion/target/debug/.cargo-lock 	new file:   Hclient_RustVersion/target/debug/.fingerprint/Hclient_RustVersion-dec72ae7885c8804/bin-Hclient_RustVersion 	new file:   Hclient_RustVersion/target/debug/.fingerprint/Hclient_RustVersion-dec72ae7885c8804/bin-Hclient_RustVersion.json 	new file:   Hclient_RustVersion/target/debug/.fingerprint/Hclient_RustVersion-dec72ae7885c8804/dep-bin-Hclient_RustVersion 	new file:   Hclient_RustVersion/target/debug/.fingerprint/Hclient_RustVersion-dec72ae7885c8804/invoked.timestamp 	new file:   Hclient_RustVersion/target/debug/.fingerprint/Hclient_RustVersion-ef493181da4d6f36/dep-test-bin-Hclient_RustVersion 	new file:   Hclient_RustVersion/target/debug/.fingerprint/Hclient_RustVersion-ef493181da4d6f36/invoked.timestamp 	new file:   Hclient_RustVersion/target/debug/.fingerprint/Hclient_RustVersion-ef493181da4d6f36/test-bin-Hclient_RustVersion 	new file:   Hclient_RustVersion/target/debug/.fingerprint/Hclient_RustVersion-ef493181da4d6f36/test-bin-Hclient_RustVersion.json 	new file:   Hclient_RustVersion/target/debug/deps/Hclient_RustVersion-dec72ae7885c8804.d 	new file:   Hclient_RustVersion/target/debug/deps/Hclient_RustVersion-ef493181da4d6f36.d 	new file:   Hclient_RustVersion/target/debug/deps/libHclient_RustVersion-dec72ae7885c8804.rmeta 	new file:   Hclient_RustVersion/target/debug/deps/libHclient_RustVersion-ef493181da4d6f36.rmeta 	new file:   Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-0bwxz58gtvwyr/s-hdndq9w8j3-0j5o1sc-4f7gp2c66xxg7luslph0l61eh/dep-graph.bin 	new file:   Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-0bwxz58gtvwyr/s-hdndq9w8j3-0j5o1sc-4f7gp2c66xxg7luslph0l61eh/query-cache.bin 	new file:   Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-0bwxz58gtvwyr/s-hdndq9w8j3-0j5o1sc-4f7gp2c66xxg7luslph0l61eh/work-products.bin 	new file:   Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-0bwxz58gtvwyr/s-hdndq9w8j3-0j5o1sc.lock 	new file:   Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-3jz31jldhzw9j/s-hdndq9w8j3-01zzvcl-9mjiu5rod7qz1rd2pkzvd61v5/dep-graph.bin 	new file:   Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-3jz31jldhzw9j/s-hdndq9w8j3-01zzvcl-9mjiu5rod7qz1rd2pkzvd61v5/query-cache.bin 	new file:   Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-3jz31jldhzw9j/s-hdndq9w8j3-01zzvcl-9mjiu5rod7qz1rd2pkzvd61v5/work-products.bin 	new file:   Hclient_RustVersion/target/debug/incremental/Hclient_RustVersion-3jz31jldhzw9j/s-hdndq9w8j3-01zzvcl.lock 	new file:   Hclient_RustVersion/target/flycheck0/stderr 	new file:   Hclient_RustVersion/target/flycheck0/stdout

---
+++ Hclient_RustVersion/target/debug/deps/Hclient_RustVersion-ef493181da4d6f36.d
@@ -0,0 +1,5 @@
+/home/maxx/Hclient_RustVersion/Hclient_RustVersion/target/debug/deps/Hclient_RustVersion-ef493181da4d6f36.d: src/main.rs
+
+/home/maxx/Hclient_RustVersion/Hclient_RustVersion/target/debug/deps/libHclient_RustVersion-ef493181da4d6f36.rmeta: src/main.rs
+
+src/main.rs:
